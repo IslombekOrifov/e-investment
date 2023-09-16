2ceed1cb3d986aff4f1c936a18ef540ee5a04f57
IslombekOrifov <islombekorifov0199@gmail.com>
2023-09-16 11:30:28 +0500

    image url https

--- einvestment/einvestment/settings.py
+++ einvestment/einvestment/settings.py
@@ -245,3 +245,6 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
+
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
